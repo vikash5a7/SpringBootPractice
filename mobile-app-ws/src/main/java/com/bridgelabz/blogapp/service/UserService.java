ced65ce08ae6d75eacc73753a348e2732952f352
vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-02-15 13:06:25 +0530

    [vikash]Refactor. encripting password

--- mobile-app-ws/src/main/java/com/bridgelabz/blogapp/service/UserService.java
+++ mobile-app-ws/src/main/java/com/bridgelabz/blogapp/service/UserService.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.blogapp.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.bridgelabz.blogapp.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	public UserDto createUser(UserDto userDto);
 
 }
--- mobile-app-ws/src/main/java/com/bridgelabz/blogapp/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/bridgelabz/blogapp/service/impl/UserServiceImpl.java
@@ -2,6 +2,9 @@ package com.bridgelabz.blogapp.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.blogapp.UserRepository;
@@ -18,6 +21,8 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	Util util;
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public UserDto createUser(UserDto user) {
@@ -31,14 +36,22 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(user, userEntity);
 
 		String userPublicKey = util.generateUserId(20);
-
-		userEntity.setEncryptedPassword("test");
-
+		// EncryptedPassword for user
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		
 		userEntity.setUserId(userPublicKey);
 		UserEntity storedUserDetails = userRepository.save(userEntity);
+		
 		UserDto returnValue = new UserDto();
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
+		
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
