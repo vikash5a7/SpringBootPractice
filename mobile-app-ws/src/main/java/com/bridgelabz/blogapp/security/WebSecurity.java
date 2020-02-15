92cc051cf9ef971dde1a4e23b4a1ffddc67a977e
vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-02-15 14:32:55 +0530

    [vikash]Refactor. jwt and signing implemented

--- mobile-app-ws/src/main/java/com/bridgelabz/blogapp/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/bridgelabz/blogapp/security/WebSecurity.java
@@ -12,23 +12,21 @@ import com.bridgelabz.blogapp.service.UserService;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-	private final UserService userDetailsService ;
+	private final UserService userDetailsService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.userDetailsService = userDetailsService;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-		.antMatchers(HttpMethod.POST, "/users")
-		.permitAll()
-		.anyRequest()
-		.authenticated();
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+				.permitAll().anyRequest().authenticated().and()
+				.addFilter(new AuthenticationFilter(authenticationManager()));
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
--- mobile-app-ws/src/main/java/com/bridgelabz/blogapp/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/bridgelabz/blogapp/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.blogapp.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,20 +41,23 @@ public class UserServiceImpl implements UserService {
 		String userPublicKey = util.generateUserId(20);
 		// EncryptedPassword for user
 		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-		
+
 		userEntity.setUserId(userPublicKey);
 		UserEntity storedUserDetails = userRepository.save(userEntity);
-		
+
 		UserDto returnValue = new UserDto();
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
-		
+
 		return returnValue;
 	}
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UserEntity userEntity = userRepository.findByEmail(email);
+		if (userEntity == null)
+			throw new UsernameNotFoundException(email);
+
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
 
 }
